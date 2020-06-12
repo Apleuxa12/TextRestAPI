2647aff40d3dfc7c84a028af7c05a8c52ec27ceb
Apleuxa12 <apleuxa12@gmail.com>
2020-06-12 20:01:06 +0300

    Text entity

---
+++ src/main/java/com/ddmukhin/models/Text.java
@@ -0,0 +1,51 @@
+package com.ddmukhin.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Text {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String theme;
+
+    private String value;
+
+    public Text(){
+
+    }
+
+    public Text(String theme, String value){
+        this.theme = theme;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
