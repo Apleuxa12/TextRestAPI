c7bf8c52308f970391c5d1cdcbe3a6d642f591bb
Apleuxa12 <apleuxa12@gmail.com>
2020-06-13 15:44:24 +0300

    findManyByTheme test

--- src/test/java/com/ddmukhin/main/repositories/TextRepositoryTest.java
+++ src/test/java/com/ddmukhin/main/repositories/TextRepositoryTest.java
@@ -7,17 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TextRepositoryTest {
 
     @Autowired
@@ -27,7 +28,7 @@ class TextRepositoryTest {
     private TextRepository textRepository;
 
     @Test
-    public void simpleTextFindByIdTest(){
+    public void simpleTextFindByIdTest() {
         Text text = new Text("theme", "value");
 
         entityManager.persist(text);
@@ -39,7 +40,7 @@ class TextRepositoryTest {
     }
 
     @Test
-    public void simpleFindAllTest(){
+    public void simpleFindAllTest() {
         Text text1 = new Text("theme1", "value1");
         Text text2 = new Text("theme2", "value2");
         Text text3 = new Text("theme3", "value3");
@@ -55,7 +56,7 @@ class TextRepositoryTest {
     }
 
     @Test
-    public void findOneByThemeTextTest(){
+    public void findOneByThemeTextTest() {
         Text text1 = new Text("theme1", "value1"); //ID: 1
         Text text2 = new Text("theme2", "value2"); //ID: 2
         Text text3 = new Text("theme3", "value3"); //ID: 3
@@ -68,6 +69,24 @@ class TextRepositoryTest {
         List<Text> found = textRepository.findByTheme("theme1");
 
         assertEquals(found.get(0).getId(), 1L);
+    }
+
+    @Test
+    public void findManyByThemeTextTest() {
+        Text text1 = new Text("theme", "value1"); //ID: 1
+        Text text2 = new Text("theme", "value2"); //ID: 2
+        Text text3 = new Text("another_theme", "value3"); //ID: 3
+
+        entityManager.persist(text1);
+        entityManager.persist(text2);
+        entityManager.persist(text3);
+        entityManager.flush();
+
+        List<Text> found = textRepository.findByTheme("theme");
+
+        assertEquals(found.size(), 2);
+        assertEquals(found.get(0).getId(), 1L);
+        assertEquals(found.get(1).getId(), 2L);
     }
 
 }
